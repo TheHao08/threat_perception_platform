e33121e82ad80d17b6d2ef557729825d83e48d65
nice020310 <1490166708@qq.com>
2025-06-10 20:44:43 +0800

    Add files via upload

---
+++ src/main/java/com/tpp/threat_perception_platform/service/AppService.java
@@ -0,0 +1,7 @@
+package com.tpp.threat_perception_platform.service;
+
+import com.tpp.threat_perception_platform.pojo.App;
+
+public interface AppService {
+    int save(App app);
+}
---
+++ src/main/java/com/tpp/threat_perception_platform/service/ServicesService.java
@@ -0,0 +1,7 @@
+package com.tpp.threat_perception_platform.service;
+
+import com.tpp.threat_perception_platform.pojo.Services;
+
+public interface ServicesService {
+    int saveServices(Services services);
+}
