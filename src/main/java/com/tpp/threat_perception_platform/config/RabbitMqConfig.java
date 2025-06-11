4309e0475115d28b2089f7fc5e041e9fea631e42
lyh <3195144473@qq.com>
2025-06-11 18:26:13 +0800

    解决冲突

--- src/main/java/com/tpp/threat_perception_platform/config/RabbitMqConfig.java
+++ src/main/java/com/tpp/threat_perception_platform/config/RabbitMqConfig.java
@@ -4,13 +4,9 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-//*这是一个RabbitMQ配置类，用于配置RabbitTemplate Bean。
-
 @Configuration
-public class RabbitMqConfig {
-
-    @Bean//bean的作用是将一个对象注册到Spring容器中，方便其他组件进行依赖注入
+public class RabbitMQConfig {
+    @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory factory){
         return new RabbitTemplate(factory);
     }
