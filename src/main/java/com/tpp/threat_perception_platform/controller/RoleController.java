b00eb1f4414b74f022d4cfd078b0703329d39c93
nice020310 <1490166708@qq.com>
2025-06-11 20:31:38 +0800

    Add files via upload

--- src/main/java/com/tpp/threat_perception_platform/controller/RoleController.java
+++ src/main/java/com/tpp/threat_perception_platform/controller/RoleController.java
@@ -4,58 +4,34 @@ import com.tpp.threat_perception_platform.param.MyParam;
 import com.tpp.threat_perception_platform.pojo.Role;
 import com.tpp.threat_perception_platform.response.ResponseResult;
 import com.tpp.threat_perception_platform.service.RoleService;
-import com.tpp.threat_perception_platform.service.impl.RoleServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class RoleController {
 
     @Autowired
     private RoleService roleService;
-    /**
-     * 角色列表
-     * @param param
-     * @return
-     */
+
     @PostMapping("/role/list")
-    public ResponseResult roleList(MyParam param){
+    public ResponseResult roleList(MyParam param) {
         return roleService.roleList(param);
     }
 
-    /**
-     * 添加用户
-     * @param role
-     * @return
-     */
     @PostMapping("/role/save")
-    public ResponseResult roleSave(@RequestBody Role role){
+    public ResponseResult save(@RequestBody Role role) {
         return roleService.save(role);
     }
-    /**
-     * 编辑用户
-     * @param role
-     * @return
-     */
+
     @PostMapping("/role/edit")
-    public ResponseResult roleEdit(@RequestBody Role role){
+    public ResponseResult edit(@RequestBody Role role) {
         return roleService.edit(role);
     }
 
-    /**
-     * 删除用户
-     * @param
-     * @return
-     */
     @PostMapping("/role/delete")
-    public ResponseResult roleEdit(@RequestParam("ids[]") Integer[] ids){
+    public ResponseResult delete(@RequestParam("ids[]") List<Long> ids) {
         return roleService.delete(ids);
     }
-
-
-
-
 }
